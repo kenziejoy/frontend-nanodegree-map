ed3f0f46ac05893d2783cdf0a700f491d2ca1470
MacKenzie Rawcliffe <kenziejoy@gmail.com>
2016-01-04 22:52:16 -0800

    fix: separated latlng, added categories

--- main.js
+++ main.js
@@ -6,9 +6,77 @@ function initMap() {
 		scrollwheel: false,
 		zoom: 16
 		});
-
+}
 	setMarkers(map);
-	}
+
+//**************Model***************
+var categories = ['all', 'eat', 'drink', 'soak', 'create', 'see'];
+
+var places = [
+		{
+			name: 'Alberta Co-op',
+			categories: ['all','eat','drink'],
+			lat: 45.5589522,
+			long: -122.6517163,
+			what: 'Yummy, fresh and good for the soul groceries',
+		},{
+			name: 'Alberta Rose Theatre  ',
+			categories: ['all','see'],
+			lat: 45.5588269,
+			long: -122.6367732,
+			what: 'Spectacular events',
+		},{
+			name: 'Bolt',
+			categories: ['all','create'],
+			lat: 45.5588854,
+			long: -122.6696039,
+			what: 'Fabrics, notions and patterns',
+		},{
+			name: 'Collage',
+			categories: ['all','create'],
+			lat: 45.5591757,
+			long: -122.6500247,
+			what: 'All the crafts!',
+		},{
+			name: 'Common Ground',
+			categories: ['all','soak'],
+			lat: 45.5592984,
+			long: -122.6304464,
+			what: 'Naked outdoor hot tubs',
+		},{
+			name: 'Cruz Room',
+			categories: ['all','eat','drink'],
+			lat: 45.5592015,
+			long: -122.646342,17,
+			what: 'Tacos, drink and funky fresh',
+		},{
+			name: 'Just Bob',
+			categories: ['all','eat','drink'],
+			lat: 45.5591434,
+			long: -122.6430461,
+			what: 'Handpies, music, drink and comfy chairs',
+		},{
+			name: 'Pine State Biscuits',
+			categories: ['all','eat','drink'],
+			lat: 45.5592015,
+			long: -122.646342,
+			what: 'Great food',
+		},{
+			name: 'Salt & Straw',
+			categories: ['all','eat','drink'],
+			lat: 45.5592015,
+			long: -122.646342,
+			what: 'The most creative flavors of ice cream',
+		}
+	]
+//**************ViewModel************
+function ViewModel() {
+	var self = this;
+
+	self.mapUnavailable = ko.observable(false);
+	self.albertaPlaces = ko.observableArray(places);
+};
+
 function setMarkers(map) {
 	// Adds markers to the map.
 	for (var i = 0; i < self.albertaPlaces.length; i++) {
@@ -19,53 +87,6 @@ function setMarkers(map) {
 			});
 		}
 	}
-//**************Model***************
-var categories = ['all', 'eat', 'drink', 'soak', 'create', 'see'];
-
-
-//**************ViewModel************
-function ViewModel() {
-	var self = this;
-	self.albertaPlaces = ko.observableArray([
-		{
-			name: 'Alberta Co-op',
-			latlng:['45.5589522','-122.6517163'],
-			what: 'Yummy, fresh and good for the soul groceries',
-		},{
-			name: 'Alberta Rose Theatre  ',
-			latlng:['45.5588269','-122.6367732'],
-			what: 'Spectacular events',
-		},{
-			name: 'Bolt',
-			latlng:['45.5588854','-122.6696039'],
-			what: 'Fabrics, notions and patterns',
-		},{
-			name: 'Collage',
-			latlng:['45.5591757','-122.6500247'],
-			what: 'All the crafts!',
-		},{
-			name: 'Common Ground',
-			latlng:['45.5592984','-122.6304464'],
-			what: 'Naked outdoor hot tubs',
-		},{
-			name: 'Cruz Room',
-			latlng:['45.5592015','-122.646342,17'],
-			what: 'Tacos, drink and funky fresh',
-		},{
-			name: 'Just Bob',
-			latlng:['45.5591434','-122.6430461'],
-			what: 'Handpies, music, drink and comfy chairs',
-		},{
-			name: 'Pine State Biscuits',
-			latlng:['45.5592015','-122.646342'],
-			what: 'Great food',
-		},{
-			name: 'Salt & Straw',
-			latlng:['45.5592015','-122.646342'],
-			what: 'The most creative flavors of ice cream',
-		}
-	]);
-};
 
 var viewModel = new ViewModel();
 ko.applyBindings(viewModel);
