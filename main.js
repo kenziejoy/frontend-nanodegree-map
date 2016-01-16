370f910a11dc42536bbc4c33b357d6b03d434225
MacKenzie Rawcliffe <kenziejoy@gmail.com>
2016-01-15 20:56:34 -0800

    fix: all the little jslint problems

--- main.js
+++ main.js
@@ -4,79 +4,88 @@ var model = [
 		name: 'Alberta Co-op',
 		lat: 45.5589522,
 		lng: -122.6517163,
-		latLng: {lat: 45.5589522, lng: -122.6517163},
-	}, {
+		latLng: {lat: 45.5589522, lng: -122.6517163}
+	},
+	{
 		name: 'Alberta Rose Theatre',
 		lat: 45.5588269,
 		lng: -122.6367732,
-		latLng: {lat: 45.5588269, lng: -122.6367732 },
-	}, {
+		latLng: {lat: 45.5588269, lng: -122.6367732 }
+	},
+	{
 		name: 'Bolt',
 		lat: 45.5589988,
 		lng: -122.6430478,
-		latLng: {lat: 45.5589988, lng: -122.6430478 },
-	}, {
+		latLng: {lat: 45.5589988, lng: -122.6430478 }
+	},
+	{
 		name: 'Collage',
 		lat: 45.559221,
 		lng: -122.6479731,
-		latLng: {lat: 45.559221, lng: -122.6479731},
-	}, {
+		latLng: {lat: 45.559221, lng: -122.6479731}
+	},
+	{
 		name: 'Common Ground',
 		lat: 45.5592984,
 		lng: -122.6304464,
-		latLng: {lat: 45.5592984, lng: -122.6304464},
-	}, {
+		latLng: {lat: 45.5592984, lng: -122.6304464}
+	},
+	{
 		name: 'Cruz Room',
 		lat: 45.5590117,
 		lng: -122.6412912,
-		latLng: {lat: 45.5590117, lng: -122.6412912},
-	}, {
+		latLng: {lat: 45.5590117, lng: -122.6412912}
+	},
+	{
 		name: 'Just Bob',
 		lat: 45.5591934,
 		lng: -122.6409898,
-		latLng: {lat: 45.5591934, lng: -122.6409898},
-	}, {
+		latLng: {lat: 45.5591934, lng: -122.6409898}
+	},
+	{
 		name: 'Salt & Straw',
 		lat: 45.5592398,
 		lng: -122.6442831,
-		latLng: {lat: 45.5592398, lng: -122.6442831},
+		latLng: {lat: 45.5592398, lng: -122.6442831}
 	}];
 
 /****************MAP**********************/
 var mapOptions = {
 	    zoom: 16,
-	    center: {lat:45.5590561, lng:-122.6447018},
+	    center: {lat: 45.5590561, lng: -122.6447018},
 	    mapTypeId: google.maps.MapTypeId.ROADMAP,
 	    disableDefaultUI: true,
 		scrollwheel: false,
-		panControl:false,
-		zoomControl:false,
-		mapTypeControl:false,
-		scaleControl:false,
-		streetViewControl:true,
-		overviewMapControl:false,
-		rotateControl:false,
-		styles: 	[{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}]
+		panControl: false,
+		zoomControl: false,
+		mapTypeControl: false,
+		scaleControl: false,
+		streetViewControl: true,
+		overviewMapControl: false,
+		rotateControl: false,
+		styles: [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}]
 	  };
 var mapDiv = document.getElementById("mapDiv");
+var map;
 
 // create map
 function initMap() {
-	self.googleMap = new google.maps.Map(mapDiv, mapOptions);
+	map = new google.maps.Map(mapDiv, mapOptions);
 }
 
 /******************VIEW MODEL***************************/
 
-var ViewModel = function() {
+var ViewModel = function () {
 
 	var self = this;
 
 	//variables
-	var contentWindow = '';
+	var contentWindow, content, Place;
 	var image = 'artsy.png';
+	var googleMap = map;
 
 	//array to store data
-	self.allPlaces=[];
+	self.allPlaces = [];
 
 	// For each object in model.locations
 		model.forEach(function(place) {
@@ -86,12 +95,10 @@ var ViewModel = function() {
 	/**********FourSquare***************/
 	// Some variables to be used for the Foursuare api
 	var latlng = '';
-	client_id = '3SHNM1LPOMY3CXWGFPDTAH3WP31ZSIEMWIY3UTUYVDMUPSSD';
-	client_secret = 'RBLLKYWKSTAUXJVKLSA42VX4LQ4ANYRCUBPRY1AQ1EOLY4C4';
-	fUrl = 'https://api.foursquare.com/v2/venues/search?ll='+ latlng +'&client_id='+ client_id +'&client_secret='+ client_secret +'&v=20151259&m=foursquare&links';
-	fquery = 'restaurant';
-	var lat;
-	var lng;
+	var client_id = '3SHNM1LPOMY3CXWGFPDTAH3WP31ZSIEMWIY3UTUYVDMUPSSD';
+	var client_secret = 'RBLLKYWKSTAUXJVKLSA42VX4LQ4ANYRCUBPRY1AQ1EOLY4C4';
+	var fUrl = 'https://api.foursquare.com/v2/venues/search?ll='+ latlng +'&client_id='+ client_id +'&client_secret='+ client_secret +'&v=20151259&m=foursquare&links';
+	var lat, lng;
 	infowindow = new google.maps.InfoWindow();
 
 	// For each object in allPlaces create map markers and infowindows
@@ -99,29 +106,25 @@ var ViewModel = function() {
 		lat = place.lat;
 		lng = place.lng;
 	  	var drop = google.maps.Animation.DROP;
-		markerOptions = {
+		var markerOptions = {
 			map: self.googleMap,
 			position: place.latLng
 		};
+		var contentName = '<h3>'+model[i].name+'</h3>';
 
 		// Sends a request to foursquare
-		$.getJSON('https://api.foursquare.com/v2/venues/search?ll='+ lat+','+lng +'&query='+ fquery +'&limit=1&client_id='+ client_id +'&client_secret='+ client_secret +'&v=20151259&m=foursquare',
+		$.getJSON('https://api.foursquare.com/v2/venues/search?ll='+ lat+','+lng +'&limit=1&client_id='+ client_id +'&client_secret='+ client_secret +'&v=20151259&m=foursquare',
 
 		// This function takes the foursquare data and processes it.
 		function(data) {
 			$.each(data.response.venues, function(i,venues){
-				content = '<hr></h5><p><sup>nearest restaurant</sup></p>' +
-				'<h5>'+ venues.name + '</h5>' +
-				'<p>'+ venues.location.formattedAddress[0] +'<br>' +
-				venues.location.formattedAddress[1] +'</p>';
+				content = '<h2>' + contentName + '</h2>' +'<hr>' + '<p>'+ venues.location.formattedAddress[0] + ' ' + venues.location.formattedAddress[1] +'</p>';
 				contentWindow = content;
-		});
+			});
 		}).fail(function(){
 			content = 'There was an error loading foursquare';
 			contentWindow = content;
-		});
-
-		var contentName = '<h4>'+model[i].name+'</h4>';
+			});
 
 		place.marker = new google.maps.Marker(markerOptions);
 		var marker = place.marker;
@@ -143,10 +146,10 @@ var ViewModel = function() {
 		})(marker));
 
 		// The click function triggers map markers.
-		clicker = function(){
+		function clicky(){
 			google.maps.event.trigger(this.marker, 'click');
 			$('#menu-toggle').trigger('click');
-		};
+		}
 
 		// Toggle visibility of list view in mobile view
 		self.Show = ko.observable(false);
