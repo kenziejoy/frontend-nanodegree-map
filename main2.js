24dba404b24714ff3b6fa05b0a3d59ebeed63228
MacKenzie Rawcliffe <kenziejoy@gmail.com>
2016-01-26 17:06:32 -0800

    fix:typo

--- main2.js
+++ main2.js
@@ -137,7 +137,7 @@ var ViewModel = function () {
 			dataType: 'json',
 			data: 'limit=1' +
 					'&ll='+ placeItem.lat() +','+ placeItem.lng() +
-					'&?client_id='+ CLIENT_ID +
+					'&client_id='+ CLIENT_ID +
 					'&client_secret='+ CLIENT_SECRET +
 					'&v=20140806' +
 					'&m=foursquare',
